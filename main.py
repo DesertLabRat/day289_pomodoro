d9040dd1ea419a892ffa0af88484249cac42e602
DesertBorn95 <delaney.seeburger@gmail.com>
2026-08-03 15:41:18 -0700

    second hour - course work finished

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import math
 
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
@@ -6,23 +7,58 @@ RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 25
-SHORT_BREAK_MIN = 5
-LONG_BREAK_MIN = 20
+WORK_MIN = 0.5
+SHORT_BREAK_MIN = 0.05
+LONG_BREAK_MIN = 0.1
+reps = 0
+timer = None
 
 # ---------------------------- TIMER RESET ------------------------------- # 
+def reset_timer():
+    global reps
+    window.after_cancel(timer)
+    timer_label.config(text="Timer", fg=GREEN)
+    canvas.itemconfig(timer_text, text="00:00")
+    tally_label.config(text="")
+    reps = 0
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 def start_timer():
-    countdown(5)
+    global reps
+    reps += 1
+
+    if reps % 8 == 0:
+        countdown(LONG_BREAK_MIN * 60)
+        timer_label.config(text="Break", fg=RED)
+    elif reps % 2 == 0:
+        countdown(SHORT_BREAK_MIN * 60)
+        timer_label.config(text="Break", fg=PINK)
+    else:
+        countdown(WORK_MIN * 60)
+        timer_label.config(text="Work", fg=GREEN)
+
+
+
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 def countdown(count):
 
+    count_minute = math.floor(count / 60)
+    count_seconds = count % 60
+    if count_seconds < 10:
+        count_seconds = f"0{count_seconds}"
 
-
-    canvas.itemconfig(timer_text, text=count)
+    canvas.itemconfig(timer_text, text=f"{count_minute}:{count_seconds}")
     if count > 0:
-        window.after(1000, countdown, count - 1)
+        global timer
+        timer = window.after(1000, countdown, count - 1)
+    else:
+        start_timer()
+        if reps % 2 == 0:
+            marks = ""
+            work_sess = math.floor(reps / 2)
+            for x in range(work_sess):
+                marks += "🗸"
+            tally_label.config(text=marks)
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -45,10 +81,10 @@ start_button.grid(column=0, row=2)
 pause_button = Button(text="Pause", font=(FONT_NAME, 16))
 pause_button.grid(column=1, row=2)
 
-reset_button = Button(text="Reset", font=(FONT_NAME, 16))
+reset_button = Button(text="Reset", font=(FONT_NAME, 16), command=reset_timer)
 reset_button.grid(column=2, row=2)
 
-tally_label = Label(text="✔", font=(FONT_NAME, 18), bg=YELLOW, fg=GREEN)
+tally_label = Label(font=(FONT_NAME, 18), bg=YELLOW, fg=GREEN)
 tally_label.grid(column=1, row=3, pady=(10, 10))
 
 
